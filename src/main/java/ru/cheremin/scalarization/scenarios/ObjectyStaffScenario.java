40429cc4f1da69fd05416836a4e9593f5a37550c
BegemoT <cheremin@gmail.com>
2016-02-18 00:48:33 +0300

    + ObjectyStaff + PrimitiveArrayAllocator

---
+++ src/main/java/ru/cheremin/scalarization/scenarios/ObjectyStaffScenario.java
@@ -0,0 +1,79 @@
+package ru.cheremin.scalarization.scenarios;
+
+/**
+ * 1.8.0_73:
+ * new Object().hashCode() -> is not scalarized
+ * new WithCustomHashCode() -> is scalarized
+ * new WithoutCustomHashCode() -> is not scalarized
+ * System.identityHashCode( new WithCustomHashCode(..) ) -> not scalarized
+ * new WithCustomHashCode() == new WithCustomHashCode() -> scalarized
+ * WithoutCustomHashCode.equals(WithoutCustomHashCode) -> scalarized
+ *
+ * I.e. it is only system identity hash code which blocks scalarization (most probably
+ * because it is native function which is not inlined, and not explicitly marked
+ * as none-escaping, as many other intrinsics does).
+ *
+ * TODO check 1.7
+ *
+ * @author ruslan
+ *         created 18/02/16 at 00:36
+ */
+public class ObjectyStaffScenario extends AllocationScenario {
+	@Override
+	public long allocate() {
+		final WithoutCustomHashCode o1 = new WithoutCustomHashCode( 1, 4 );
+		final WithoutCustomHashCode o2 = new WithoutCustomHashCode( 1, 4 );
+		return o1.equals( o2 ) ? 1 : 5;
+	}
+
+	public static final class WithCustomHashCode {
+		private double x;
+		private double y;
+
+		public WithCustomHashCode( final double x,
+		                           final double y ) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public boolean equals( final Object o ) {
+			if( this == o ) {
+				return true;
+			}
+			if( o == null || getClass() != o.getClass() ) {
+				return false;
+			}
+
+			final WithCustomHashCode that = ( WithCustomHashCode ) o;
+
+			if( Double.compare( that.x, x ) != 0 ) {
+				return false;
+			}
+			return Double.compare( that.y, y ) == 0;
+
+		}
+
+		@Override
+		public int hashCode() {
+			int result;
+			long temp;
+			temp = Double.doubleToLongBits( x );
+			result = ( int ) ( temp ^ ( temp >>> 32 ) );
+			temp = Double.doubleToLongBits( y );
+			result = 31 * result + ( int ) ( temp ^ ( temp >>> 32 ) );
+			return result;
+		}
+	}
+
+	public static final class WithoutCustomHashCode {
+		private double x;
+		private double y;
+
+		public WithoutCustomHashCode( final double x,
+		                              final double y ) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
--- src/main/java/ru/cheremin/scalarization/scenarios/PrimitiveArrayAllocator.java
+++ src/main/java/ru/cheremin/scalarization/scenarios/PrimitiveArrayAllocator.java
@@ -1,23 +1,92 @@
 package ru.cheremin.scalarization.scenarios;
 
 /**
+ * Both 1.8.0_73 and 1.7.0_25 JVMs:
+ *
+ * With explicit cell access, like array[4], array is scalarized if length <= 64
+ * With vectorized (looping) access array is not scalarized with length >1 (and is
+ * scalarized for length = 1).
+ * Manual loop unrolling with switch still allow to scalarize array
+ *
  * @author ruslan
  *         created 13.11.12 at 23:11
  */
 public class PrimitiveArrayAllocator extends AllocationScenario {
+	private static final boolean VECTORIZED = Boolean.getBoolean( "scenario.vectorized-array-access" );
 
 	@Override
 	public long allocate() {
 		final byte[] array = new byte[SIZE];
-		for( int i = 0; i < SIZE; i++ ) {
-			array[i] = 1;
-		}
+		fill( array, ( byte ) 42 );
 
+
+		return sum( array );
+	}
+
+	private static long sum( final byte[] array ) {
 		long sum = 0;
-		for( int i = 0; i < SIZE; i++ ) {
-			sum += array[i];
+		if( VECTORIZED ) {
+			for( final byte b : array ) {
+				sum += b;
+			}
+		} else {
+			//manual loop unrolling
+			switch( array.length ) {
+				default:
+				case 7:
+					sum += array[6];
+				case 6:
+					sum += array[5];
+				case 5:
+					sum += array[4];
+				case 4:
+					sum += array[3];
+				case 3:
+					sum += array[2];
+				case 2:
+					sum += array[1];
+				case 1:
+					sum += array[0];
+				case 0:
+					break;
+			}
 		}
-
 		return sum;
+	}
+
+	private static void fill( final byte[] array,
+	                          final byte value ) {
+		if( VECTORIZED ) {
+			for( int i = 0; i < array.length; i++ ) {
+				array[i] = value;
+			}
+		} else {
+			//manual loop unrolling
+
+			switch( array.length ) {
+				default:
+				case 7:
+					array[6] = value;
+				case 6:
+					array[5] = value;
+				case 5:
+					array[4] = value;
+				case 4:
+					array[3] = value;
+				case 3:
+					array[2] = value;
+				case 2:
+					array[1] = value;
+				case 1:
+					array[0] = value;
+				case 0:
+					break;
+			}
+		}
+	}
+
+	@Override
+	public String additionalInfo() {
+		return VECTORIZED ? "vectorized" : "manually unrolled";
 	}
 }
