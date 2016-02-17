743ff41c269115f42e03d9c67fd5e39987bff2b1
BegemoT <cheremin@gmail.com>
2016-02-18 00:14:40 +0300

    more about EqualsBuilderScenario

--- src/main/java/ru/cheremin/scalarization/scenarios/EqualsBuilderScenario.java
+++ src/main/java/ru/cheremin/scalarization/scenarios/EqualsBuilderScenario.java
@@ -6,15 +6,18 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 /**
  * With using at least one .append(Object,Object) method (even without looping) -> EA
  * fails with "Skipping method because: code size (327) exceeds MaxBCEAEstimateSize (150)"
- * on both 1.7 and 1.8 jdks. It is .append(Object,Object) which cost 327 bytecodes alone.
+ * on both 1.7 and 1.8 JVMs. It is .append(Object,Object) which cost 327 bytecodes
+ * alone.
  * <p/>
- * Even with -XX:MaxBCEAEstimateSize=1400 allocations still happen. Probably, because
- * of .getClass() intrinsic function used in .append(Object,Object), but I'm not sure
- * TODO verify with debug jvm
+ * Even with -XX:MaxBCEAEstimateSize=1400 allocations still happen. -XX:+PrintInlining
+ * produces "callee is too large" and "hot method too big" for EqualsBuilder.append(),
+ * so it looks like inlining threshold is breached. Default FreqInlineSize is 325, so
+ * it looks like it is less than 327 bc/append. Increasing -XX:FreqInlineSize 325->500
+ * indeed removes all allocations with 1.8 and 1.7 JVMs.
  * <p/>
- * Without .append(Object,Object) scalarization do happens
- * Extending EqualsBuilder to EqualsBuilderEx with specialized .append(String,String)
- * also cause scalarization to happen.
+ * Without .append(Object,Object) scalarization do happens successfully with default
+ * settings: e.g. Extending EqualsBuilder to EqualsBuilderEx with specialized short
+ * .append(String,String)method also cause scalarization to happen.
  *
  * @author ruslan
  *         created 16/02/16 at 23:45
@@ -25,7 +28,7 @@ public class EqualsBuilderScenario extends AllocationScenario {
 
 	@Override
 	public long allocate() {
-		final EqualsBuilderEx builder = new EqualsBuilderEx();
+		final EqualsBuilder builder = new EqualsBuilder();
 //		for( final String key : keys ) {
 //			builder.append( key, key );
 //		}
