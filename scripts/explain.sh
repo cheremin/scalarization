0052c32aabbcd4d0a81c2afa7346938d7ba9323b
BegemoT <cheremin@gmail.com>
2016-09-11 00:26:26 +0300

    run scripts

--- scripts/explain.sh
+++ scripts/explain.sh
@@ -1,8 +1,11 @@
 
-# -XX:CompileCommand="print ru/cheremin/scalarization/scenarios/$SCENARIO run"
-# -XX:BCEATraceLevel=3
-# -XX:+PrintEscapeAnalysis (non-product)
-# -XX:+Verbose             (non-product)
+# -XX:CompileCommand="print ru/cheremin/scalarization/lab/$SCENARIO run"
+# -XX:BCEATraceLevel=3      (inter-procedural EA)
+# Debug JMV options
+# -XX:+PrintEscapeAnalysis  (non-product)
+# -XX:+Verbose              (non-product)
+# -XX:+TraceDeoptimizations (non-product)
+# -XX:+ProfileTraps         (non-product)
 
 # JITWatch preset:
 # -XX:-PrintInlining
@@ -11,15 +14,17 @@
 # -XX:+PrintAssembly
 # -XX:LogFile=jit.log
 
+
+
 JAVA_OPTS="$JAVA_OPTS -Xmx64m -Xms64m -XX:+UseSerialGC \
 -XX:+DoEscapeAnalysis \
 -XX:+UnlockDiagnosticVMOptions \
 -XX:+LogCompilation -XX:+TraceClassLoading -XX:+PrintAssembly -XX:-PrintInlining \
 -XX:LogFile=jit.log"
 
-JAVA_CMD="$JAVA_HOME/bin/java -cp target/benchmarks.jar $JAVA_OPTS"
+JAVA_CMD="$JAVA_HOME/bin/java -cp lab/target/benchmarks.jar $JAVA_OPTS"
 
 echo "JAVA_CMD: $JAVA_CMD:"
 echo "Explaining $@:"
 
-$JAVA_CMD $@ -XX:CompileCommand="log ru/cheremin/scalarization/scenarios/misc/PreconditionsScenario run" ru.cheremin.scalarization.infra.AllocationBenchmarkMain
+$JAVA_CMD $@ -XX:CompileCommand="log ru/cheremin/scalarization/lab/misc/PreconditionsScenario run" ru.cheremin.scalarization.infra.AllocationBenchmarkMain
--- scripts/run-all-with-j1.7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home"
-
-./run-all.sh
-
-
-
-
